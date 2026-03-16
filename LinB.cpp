f731360a47b3f25df345014708c0ffaa7a397a91
Karasik-Fish <koroleff_2000@mail.ru>
2026-03-16 21:09:25 +0300

    Update LinB.cpp

--- LinB.cpp
+++ LinB.cpp
@@ -1,6 +1,5 @@
 ﻿#include <windows.h>
 
-// Экспортируем функцию
 extern "C" __declspec(dllexport) void __stdcall GetDataFromB(wchar_t* buffer) {
     const wchar_t* msg = L"Data from LibB (Thread Safe)";
     lstrcpyW(buffer, msg);
