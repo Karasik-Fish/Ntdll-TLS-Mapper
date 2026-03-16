7e6fee60cb8ca1e847848894e5cc77242676b9d5
Karasik-Fish <koroleff_2000@mail.ru>
2026-03-16 21:08:56 +0300

    Update LibC.cpp

--- LibC.cpp
+++ LibC.cpp
@@ -4,27 +4,22 @@
 
 extern "C" __declspec(dllimport) void __stdcall GetDataFromB(wchar_t* buffer);
 
-// Структура для передачи данных в поток
 struct ThreadData {
     wchar_t path[MAX_PATH];
 };
 
-// Функция потока
 DWORD WINAPI ThreadProc(LPVOID lpParam) {
     ThreadData* data = (ThreadData*)lpParam;
     wchar_t buffer[256];
 
-    // Вызов функции из зависимой DLL внутри потока
     GetDataFromB(buffer);
 
-    // Выведем сообщение (для теста) или запишем лог
     MessageBoxW(NULL, buffer, L"Thread from LibC", MB_OK);
 
     delete data;
     return 0;
 }
 
-// Экспортируемая функция для запуска теста
 extern "C" __declspec(dllexport) void __stdcall RunThreadTest() {
     ThreadData* data = new ThreadData();
     CreateThread(NULL, 0, ThreadProc, data, 0, NULL);
